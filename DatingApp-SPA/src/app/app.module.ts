617ff7945f5b7c9622abf9a99f30320270bfd850
Andreas Hilfling Ruud <andruud@hotmail.com>
2020-05-25 10:05:21 +0200

    End of section 7

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 
@@ -13,12 +14,37 @@ import { HomeComponent } from './home/home.component';
 import { AuthService } from './_services/auth.service';
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+import { appRoutes } from './routes';
+
 
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, RegisterComponent, HomeComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule, BsDropdownModule.forRoot(), BrowserAnimationsModule],
-  providers: [AuthService, ErrorInterceptorProvider],
-  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    RegisterComponent,
+    HomeComponent,
+    MemberListComponent,
+    ListsComponent,
+    MessagesComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot(),
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider
+  ],
+  bootstrap: [
+    AppComponent
+  ],
 })
 export class AppModule { }
